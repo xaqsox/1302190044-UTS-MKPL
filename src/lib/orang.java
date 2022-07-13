4f4cb208127b24bcd422725ecc86c8d3a93f69d8
Mochamad Aqsa <101115008+xaqsox@users.noreply.github.com>
2022-07-13 08:29:13 +0700

    re:make (create class people

---
+++ src/lib/orang.java
@@ -0,0 +1,10 @@
+	package lib;
+
+public class Person {
+  private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+  
+}
